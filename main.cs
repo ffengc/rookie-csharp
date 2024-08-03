23842efb7a9d919dc86bc56549e2937ed6003513
ffengc <yufc779@gmail.com>
2024-08-03 13:30:21 +0800

    first test

---
+++ main.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using rookie_csharp.src;
+using System;
+namespace HelloWorldApplication
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            /* 我的第一个 C# 程序*/
+            Console.WriteLine("Hello World");
+            test_class tc1 = new test_class();
+            Console.WriteLine(tc1.v);
+            tc1.test_f();
+        }
+    }
+}
---
+++ src/test_class.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rookie_csharp.src
+{
+    public class test_class
+    {
+        public double v = 1;
+        public void test_f()
+        {
+            Console.WriteLine("in test_f()");
+        }
+    }
+}
